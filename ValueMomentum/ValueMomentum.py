6eed4bd44dbb721994adb751fb5b31cb2b2a45be
jjviton <j3viton@gmail.com>
2025-10-04 13:51:07 +0200

    Initial commit

---
+++ ValueMomentum/ValueMomentum.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+*****************************************************************************
+.PY
+
+Programa para un estrategia robusta y basica
+OBEJTIVO: tener algo consistente que me anime en otras estrategias mas arriesgadas
+
+
+******************************************************************************
+******************************************************************************
+
+
+Mejoras:    
+
+Started on DIC/2022
+Version_1: 
+
+Objetivo: 
+
+Author: J3Viton
+
+"""
+
+
+import configKEY
+
+  
+
+
+print (configKEY.J3_OIA_KEY)
+
+print 
---
+++ ValueMomentum/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 11:31:34 2023
+
+@author: INNOVACION
+"""
+
+"""
+Projects 
+"""
